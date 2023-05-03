52ebfb0ee35229b35136d3a84085fcb6b5d92e1a
Abel Adisu <abeladisu73@gmail.com>
2023-05-03 06:20:14 -0700

    newu pupdated

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -2,4 +2,5 @@
 # This script accepts one argument and passes it to a regular expression,
 # matching method
 # The regular expression must match the given cases
-puts ARGV[0].scan(/hb?tn/).join
+
+puts ARGV[0].scan(/hbt+n/).join
